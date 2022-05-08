f8d518a12b3dd57861d642dbd99a1f30b774f2aa
lewgomz <lgluisgomez@yahoo.com>
2022-05-07 22:38:38 -0700

    Adding Grid Item sizes within Box

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import LittleBlurbs from './blurp/LittleBlurbs';
+import Box from '@mui/material/Box';
 import Blurbs from './blurp/Blurbs';
 import Avatar from '@mui/material/Avatar';
 import Grid from '@mui/material/Grid';
@@ -7,14 +8,14 @@ import Grid from '@mui/material/Grid';
 export default function Content() {
 
   return (
-    <>
+    <Box sx={{ flexGrow: 1 }}>
         <Grid container spacing={2}>
-          <Grid item xs={2}>
+          <Grid item xs={3} md={2}>
             <Avatar alt="Lewis Gomez" src="https://avatars.githubusercontent.com/u/105124673?v=4"
               sx={{ width: 120, height: 120 }}
             />
           </Grid>
-          <Grid item xs={10}>
+          <Grid item xs={9} md={10}>
               I want to add a bio here or something cool! Idk what to say but this is about me! 🐲
           </Grid>
           <Grid item xs={12}>
@@ -24,6 +25,6 @@ export default function Content() {
           <Blurbs />
           </Grid>
         </Grid>
-    </>
+    </Box>
   );
 }
--- src/components/blurp/LittleBlurb.tsx
+++ src/components/blurp/LittleBlurb.tsx
@@ -1,37 +1,38 @@
 import * as React from 'react';
 import { useParams } from "react-router-dom";
+import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
-import { BlurbService } from '../../services/BlurbSerivce';
 import Grid from '@mui/material/Grid';
+import { BlurbService } from '../../services/BlurbSerivce';
 
 export default function LittleBlurb() {
     let params = useParams();
     let blurbId = params.id as string;
     const blurb = new BlurbService().getLittleBurb(blurbId);
     return (
-        <Grid container spacing={2}>
-            {blurb && <>
-                <Grid item xs={12}>
-                <Typography variant="h3">
-                        {blurb.title}
-                    </Typography>
-                </Grid>
-                <Grid item xs={4}>
-                    <img
-                        src={`${blurb.image}?w=164&h=164&fit=crop&auto=format`}
-                        srcSet={`${blurb.image}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
-                        alt={blurb.title}
-                        loading="lazy"
-                    />
-                </Grid>
-                <Grid item xs={8}>
-                    <Typography variant="body1" paragraph>
-                        {blurb.description}
-                    </Typography>
-                </Grid>
-                
-                
-            </>}
-        </Grid>
+        <Box sx={{ flexGrow: 1 }}>
+            <Grid container spacing={2}>
+                {blurb && <>
+                    <Grid item xs={12}>
+                    <Typography variant="h3">
+                            {blurb.title}
+                        </Typography>
+                    </Grid>
+                    <Grid item xs={5} md={4}>
+                        <img
+                            src={`${blurb.image}?w=164&h=164&fit=crop&auto=format`}
+                            srcSet={`${blurb.image}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
+                            alt={blurb.title}
+                            loading="lazy"
+                        />
+                    </Grid>
+                    <Grid item xs={7} md={6}>
+                        <Typography variant="body1" paragraph>
+                            {blurb.description}
+                        </Typography>
+                    </Grid>
+                </>}
+            </Grid>
+        </Box>
     );
 }
